5b770ba5c15bd104ff562859e86126df9e1ef5a0
ducruetl <ducruet.leo1@gmail.com>
2024-12-05 23:44:22 +0100

    Added a title and an icon to the sidebar (the icon is temporary)

--- src/elements/App.jsx
+++ src/elements/App.jsx
@@ -4,21 +4,13 @@ import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="w-16" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
       <Sidebar/>
+      <div>
+         <div className='bg-gray-600 max-w-fit h-screen'>
+
+         </div>
+      </div>
     </div>
   );
 }
--- src/elements/Sidebar.jsx
+++ src/elements/Sidebar.jsx
@@ -1,7 +1,13 @@
+import logo from '../logo.svg';
+
 function Sidebar() {
 	return (
-		<div className="flex flex-col text-white bg-gray-900">
-
+		<div className="flex flex-col text-white bg-gray-900 w-72 h-screen">
+			<div className='flex text-center items-center'>
+				<img className="w-16" src={logo} alt="logo"/>
+				<h1>Productivity App</h1>
+			</div>
+			<hr className='border-gray-700' />
 		</div>
 	);
 }
